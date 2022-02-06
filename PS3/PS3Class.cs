7aa3841598c9f53a80a01954f396b184254074a1
BrittonJordan <63472260+BrittonJordan@users.noreply.github.com>
2022-02-05 17:12:53 -0700

    Add project files.

---
+++ PS3/PS3Class.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace PS3
+{
+    public class PS3Class
+    {
+        static void Main(string[] args)
+        {
+            PS3Class p = new PS3Class();
+            p.run();
+        }
+
+        public void run()
+        {
+
+        }
+
+        public virtual string ReadLine()
+        {
+            return Console.ReadLine();
+        }
+
+        public virtual void WriteLine(string s)
+        {
+            Console.WriteLine(s);
+        }
+    }
+}
---
+++ PS3_Tester/Tests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using PS3;
+
+namespace PS3_Tester
+{
+    class Tests: PS3Class
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public override string ReadLine()
+        {
+            //if (firstRead)
+            //{
+            //    firstRead = false;
+            //    return solarData.Length.ToString();
+            //}
+            //return solarData[currentIndex].ToString();
+            return "";
+        }
+
+        public override void WriteLine(string s)
+        {
+            //string[] elements = s.Split(' ');
+            //if (elements[0] == "minimum")
+            //{
+            //    Console.WriteLine(elements[1] + " " + elements[2]);
+            //}
+            //else
+            //{
+            //    currentIndex = Int32.Parse(elements[1]);
+            //}
+        }
+    }
+}
