2e7e0633f9c7762e7ad7864216bc07176b95c1ed
BrittonJordan <63472260+BrittonJordan@users.noreply.github.com>
2022-02-09 00:05:23 -0700

    done but not working with autograder

--- PS3/PS3Class.cs
+++ PS3/PS3Class.cs
@@ -4,6 +4,12 @@ namespace PS3
 {
     public class PS3Class
     {
+        private long[] connections; // holds numDestinations bitwise set inclusion representations (each is numDestinations long)
+        private long idealSol;
+        private long everyIsland;
+        private int idealIslandCount;
+        private int numDestinations;
+
         static void Main(string[] args)
         {
             PS3Class p = new PS3Class();
@@ -12,17 +18,100 @@ namespace PS3
 
         public void run()
         {
-            int numDestinations = Int32.Parse(ReadLine());
+            //WriteLine("test123");
+
+            numDestinations = Int32.Parse(ReadLine());
             int numRoutes = Int32.Parse(ReadLine());
 
-            for(int i = 0; i < numRoutes; i++)
+            connections = new long[numDestinations + 1];  // ignore index 0
+            everyIsland = 0;
+            idealIslandCount = numDestinations;
+
+            // Every island is "connected" to itself
+            for(int i = 1; i < numDestinations + 1; i++)
+            {
+                //connections[i] = connections[i] | ((long)1 << (i - 1));  // Took this out to implement optimization
+
+                // Compose bitwise representation of every island included in set
+                everyIsland = (long)1 | everyIsland << 1;  // 11111111
+            }
+
+            for (int i = 0; i < numRoutes; i++)
             {
                 string pair = ReadLine();
                 string[] elements = pair.Split(' ');
                 int dest1 = Int32.Parse(elements[0]);
                 int dest2 = Int32.Parse(elements[1]);
+
+                // update the connection at connections[dest1]
+                connections[dest1] = connections[dest1] | ((long)1 << (dest2 - 1));
+
+                // update the connection at connections[dest2]
+                connections[dest2] = connections[dest2] | ((long)1 << (dest1 - 1));
             }
 
+            long partialSolution = 0; // 000000
+            int currIsland = 0;
+
+
+            buildShop(partialSolution, currIsland);
+
+            WriteLine(idealIslandCount.ToString());
+
+            string IdealSolution = Convert.ToString(idealSol, 2);
+            string islands = "";
+            for (int i = 1; i < IdealSolution.Length + 1; i++)
+            {
+                int currChar = IdealSolution.Length - i;
+                if (IdealSolution[currChar] == '1')
+                {
+                    islands += i.ToString() + " ";
+                }
+            }
+            islands = islands.Substring(0, islands.Length - 1);
+
+            WriteLine(islands);
+        }
+
+        public void buildShop(long partialSol, int currIsland)
+        {
+            currIsland++;
+            if (currIsland > numDestinations)
+            {
+                checkIfIdealSol(partialSol);
+                return;
+            }
+
+            long partialSolYes = partialSol | ((long)1 << (currIsland - 1));
+            buildShop(partialSolYes, currIsland);  // Yes, build a shop on currIsland
+
+            if(connections[currIsland] != 0)
+                buildShop(partialSol, currIsland); // No, don't build a shop on currIsland
+
+            //WriteLine("Done with level " + currIsland);
+        }
+
+        public void checkIfIdealSol(long sol)
+        {
+            int islandCount = 0;
+            string solution = Convert.ToString(sol, 2);
+            long currentCoveredIslands = sol;
+
+            for(int i = 1; i < solution.Length + 1; i++)
+            {
+                int currChar = solution.Length - i;
+                if(solution[currChar] == '1')
+                {
+                    islandCount++;
+                    currentCoveredIslands = currentCoveredIslands | (connections[i] << 0);   // Partial solution and the connection of every island in partial solution should touch every island
+                }
+            }
+
+            if(currentCoveredIslands == everyIsland && islandCount <= idealIslandCount)
+            {
+                idealIslandCount = islandCount;
+                idealSol = sol;
+            }
         }
 
         public virtual string ReadLine()
--- PS3_Tester/Tests.cs
+++ PS3_Tester/Tests.cs
@@ -3,35 +3,89 @@ using PS3;
 
 namespace PS3_Tester
 {
+    public class Route
+    {
+        public int one;
+        public int two;
+        public Route(int o, int t)
+        {
+            one = o;
+            two = t;
+        }
+    }
+
     class Tests: PS3Class
     {
+
+        private int numDest;
+        private Route[] routes;
+        bool readDest;
+        bool readRouteNum;
+        int currRoute;
+
+        public Tests()
+        {
+            readDest = false;
+            readRouteNum = false;
+            currRoute = 0;
+        }
+
+        public void generateRoutes(int numRoutes, int numDestinations)
+        {
+            Route[] rs = new Route[numRoutes];
+            Random r = new Random();
+            for (int i = 0; i < numRoutes; i++)
+            {
+                //rs.SetValue(new Route(r.Next(1, numDestinations), r.Next(1, numDestinations)), i);
+                int from = r.Next(1, numDestinations);
+                int to = r.Next(1, numDestinations);
+                while(to == from)
+                {
+                    to = r.Next(1, numDestinations);
+                }
+
+                rs[i] = new Route(from, to);
+                //rs[i].one = r.Next(1, numDestinations);
+                //rs[i].two = r.Next(1, numDestinations);
+            }
+
+            this.routes = rs;
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+            Tests t = new Tests();
+            t.numDest = 10;
+            t.generateRoutes(11, t.numDest);
+            t.run();
+
         }
 
         public override string ReadLine()
         {
-            //if (firstRead)
-            //{
-            //    firstRead = false;
-            //    return solarData.Length.ToString();
-            //}
-            //return solarData[currentIndex].ToString();
-            return "";
+            if (!readDest)
+            {
+                readDest = true;
+                return numDest.ToString();
+            }
+            else if (!readRouteNum)
+            {
+                readRouteNum = true;
+                return routes.Length.ToString();
+            }
+            else
+            {
+                string route = "";
+                route += routes[currRoute].one.ToString() + " " + routes[currRoute].two.ToString();
+                currRoute++;
+                return route;
+            }
         }
 
         public override void WriteLine(string s)
         {
-            //string[] elements = s.Split(' ');
-            //if (elements[0] == "minimum")
-            //{
-            //    Console.WriteLine(elements[1] + " " + elements[2]);
-            //}
-            //else
-            //{
-            //    currentIndex = Int32.Parse(elements[1]);
-            //}
+            Console.WriteLine(s);
         }
     }
 }
